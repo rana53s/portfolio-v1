05e40540efea4e57b53b3ca6bbb57fe160cd6679
Rana Sarkar <rana.sarkarg3@gmail.com>
2022-03-22 23:53:48 +0530

    hamburger
    hamburger

--- index.js
+++ index.js
@@ -5,11 +5,4 @@ function toggleButton() {
     navList.classList.toggle('show')
 }
 
-// hamburgerButton.addEventListener('click', toggleButton);
-
-// var typed = new Typed(".typing", {
-//     strings: ["a full stack web developer", "Learn to love new technologies", "Love to travel"],
-//     typeSpeed: 70,
-//     backSpeed: 20,
-//     loop: true
-// });
+hamburgerButton.addEventListener('click', toggleButton);
